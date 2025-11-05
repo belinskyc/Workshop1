ff9328f7d45fbf9a243b96f93dc8db0e57a993cd
belinskyc <36079242+belinskyc@users.noreply.github.com>
2025-11-05 12:59:32 +0700

    Update script2.R

--- scripts/script2.R
+++ scripts/script2.R
@@ -1,7 +1,6 @@
 rm(list=ls());                         # clear Environment tab
-library(package=ggplot2);              # get the GGPlot package
 
-plot5 = ggplot( data=weatherData ) +
+plot1 = ggplot( data=weatherData ) +
   geom_point( mapping=aes(x=avgTemp, y=relHum) ) +
   labs( title="Humidity vs Temperature",
         subtitle="Lansing, MI -- 2016",
@@ -10,4 +9,5 @@ plot5 = ggplot( data=weatherData ) +
   scale_x_continuous( breaks = seq(from=10, to=80, by=10) ) +
   theme_bw() +
   theme( axis.text.x=element_text(angle=90, vjust=0.5) );
-plot(plot5);
+plot(plot1);
+
